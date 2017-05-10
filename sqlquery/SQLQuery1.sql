4534208462c26d9d82029a3a5b16c46606cfa8ff
Albert Campos <acampos892@gmail.com>
2017-05-10 17:49:56 -0500

    EventPanner
    Event Planner using .NET MVC

---
+++ sqlquery/SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Event]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Title] VARCHAR(50) NULL,
+	[Location] VARCHAR(250),
+	[EventDate] DateTime NULL,
+	[EventTypeId] int NOT NULL,
+	[Price] Decimal(5,2) NOT NULL
+)
+
+CREATE TABLE [dbo].[EventType]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Label] VARCHAR(250) NULL
+)
